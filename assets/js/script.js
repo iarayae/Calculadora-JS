a1db0fe7741c1ec56e031e4be958e2d5d3cf9fc3
Ignacio <148559176+iarayae@users.noreply.github.com>
2025-05-06 01:58:23 +0000

    JS Rev01 - Validacion errores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 function obtenerNumeros() {
     const numero1 = parseFloat(document.getElementById("numero1").value);
     const numero2 = parseFloat(document.getElementById("numero2").value);
+    
+    if (numero1 === "" || numero2 === "") {
+        mostrarResultado("Por favor ingresa ambos números.");
+        throw new Error("Campos vacíos");
+    }
+    
+    const num1 = parseFloat(numero1);
+    const num2 = parseFloat(numero2);
+    
+    if (isNaN(num1) || isNaN(num2)) {
+        mostrarResultado("Debes ingresar números válidos.");
+        throw new Error("Valores no númericos");
+    }
+        
     return { numero1, numero2 };
 }
 
@@ -9,25 +23,41 @@ function mostrarResultado(valor) {
 }
 
 function sumar() {
-    const { numero1, numero2} = obtenerNumeros();
-    mostrarResultado(numero1+numero2);
+    try {
+        const { numero1, numero2} = obtenerNumeros();
+        mostrarResultado(numero1+numero2);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 function restar() {
-    const { numero1, numero2} = obtenerNumeros();
-    mostrarResultado(numero1-numero2);
+    try {
+        const { numero1, numero2 } = obtenerNumeros();
+        mostrarResultado(numero1 - numero2);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 function multiplicar() {
-    const { numero1, numero2} = obtenerNumeros();
-    mostrarResultado(numero1*numero2);
+    try {
+        const { numero1, numero2 } = obtenerNumeros();
+        mostrarResultado(numero1 * numero2);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 function dividir() {
-    const { numero1, numero2} = obtenerNumeros();
-    if (numero2==0) {
-        mostrarResultado("Error - no se puede dividir por 0");
-    } else {
-        mostrarResultado(numero1/numero2);
+    try {
+        const { numero1, numero2 } = obtenerNumeros();
+        if (numero2 === 0) {
+            mostrarResultado("Error: no se puede dividir por 0");
+        } else {
+            mostrarResultado(numero1 / numero2);
+        }
+    } catch (error) {
+        console.error(error.message);
     }
 }
